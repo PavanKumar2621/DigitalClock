584c475b5363d4b448ad8527adfb447052661768
PavanKumar2621 <madempavankumar2621@gmail.com>
2024-04-06 19:21:08 +0530

    first commit

---
+++ DigitalClock.js
@@ -0,0 +1,19 @@
+let z;
+function f()
+{
+    let d= new Date;
+    let t=d.toLocaleTimeString();
+    let x=document.getElementById("me");
+    
+    x.value=t;
+   
+}
+function start()
+{
+    z= setInterval(f,1000);
+}
+
+function stop() {
+    clearTimeout(z);
+    document.getElementById("me").value ="Time Stopped ";
+}
